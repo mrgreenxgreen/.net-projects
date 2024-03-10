a47ade8c2e6c4db0cd17969a850365610524dbe5
mrgreenxgreen <joshreyes6969@gmail.com>
2024-03-10 21:04:18 +0800

    yes

---
+++ leetcode/PalindromeNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+*/
+
+public class Solution
+{
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        Console.WriteLine("Enter number:");
+        string number = Console.ReadLine();
+        int num = Convert.ToInt32(number);
+        Console.WriteLine(solution.IsPalindrome(num));
+
+    }
+    public bool IsPalindrome(int xx)
+    {
+
+         int ReversedNumber(int x)
+        {
+            int reversedNumber = 0;
+            while (x != 0)
+            {
+                reversedNumber = reversedNumber * 10 + x % 10;
+                x /= 10;
+            }
+            if (!reversedNumber.Equals(x))
+            {
+                return reversedNumber;
+            }
+            return reversedNumber;
+        }
+        if(ReversedNumber(xx) == xx){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
+}
