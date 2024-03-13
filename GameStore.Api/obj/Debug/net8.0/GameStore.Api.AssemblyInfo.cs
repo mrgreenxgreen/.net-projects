46786c3f36b24e9f602f12d57e876fe3ab860e0d
mrgreenxgreen <joshreyes6969@gmail.com>
2024-03-14 00:21:39 +0800

    sql lite added

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47ade8c2e6c4db0cd17969a850365610524dbe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2573b870bf4c9cd96c75c7c08aa76407ba2b09a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
